d83df27548b780e0ab04530f96efa6b0b8806bd7
Jean Machado <jeanvictor.arq@gmail.com>
2021-01-20 21:12:57 -0300

    atualizando arquivo do babel

--- src/components/Input/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const Input = () => (
-  <S.Wrapper>
-    <h1>Input</h1>
-  </S.Wrapper>
-)
-
-export default Input
--- src/components/Input/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Input from '.'
-
-export default {
-  title: 'Input',
-  component: Input
-} as Meta
-
-export const Basic: Story = (args) => <Input />
